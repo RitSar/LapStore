3ad076d4503641eecae966ccd0306817f2b06109
RitSar <55022926+RitSar@users.noreply.github.com>
2020-08-21 21:03:50 +0530

    cart functional

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Title({title}) {
     return (
-        <div className="middle">
+        <div className="middle mt-3 mb-3">
             <h1 className="font-weight-bold lobster">{title}</h1>
         </div>
     )
--- src/context.js
+++ src/context.js
@@ -11,6 +11,9 @@ class ProductProvider extends Component {
         cart: [],
         modalOpen: false,
         modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0,
     };
     componentDidMount(){
         this.setProducts();
@@ -50,18 +53,88 @@ class ProductProvider extends Component {
 
         this.setState(()=>{
             return { products: tempProducts, cart:[...this.state.cart,product] };
-        },()=>{console.log(this.state);});
+        },()=>{this.addTotals();});
     };
 
     openModal = id => {
         const product = this.getItem(id);
         this.setState(()=>{
             return {modalProduct: product,modalOpen:true};
-        })
-    }
+        });
+    };
     closeModal = () => {
         this.setState(()=>{
             return {modalOpen: false}
+        });
+    };
+    increment = (id) => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        ++product.count;
+        product.total = product.price * product.count; 
+
+        this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotals()})
+    };
+    decrement = (id) => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        --product.count;
+        if(product.count === 0){
+            this.removeItem(id);
+        }else{
+            product.total = product.price * product.count;
+            this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotals()})
+        }
+    };
+    removeItem = (id) => {
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = false;
+
+        this.setState(()=>{
+            return{
+                cart: [...tempCart],
+                products: [...tempProducts],
+            }
+        },()=>{
+            this.addTotals();
+        }
+        );
+    };
+    clearCart = () => {
+        this.setState(
+            () => { return {cart:[]}; },
+            () => {
+                this.setProducts();
+                this.addTotals();
+            }
+        )
+    };
+    addTotals = () =>{
+        let subTotal = 0;
+        this.state.cart.map(item => (subTotal += item.total));
+        const tax = parseFloat((subTotal*0.17).toFixed(2));
+        const total = parseFloat((subTotal + tax).toFixed(2));
+        this.setState(()=>{
+            return {
+                cartSubTotal: subTotal,
+                cartTax: tax,
+                cartTotal: total
+            }
         })
     }
     // tester = () => {
@@ -85,6 +158,10 @@ class ProductProvider extends Component {
                 addToCart: this.addToCart,
                 openModal: this.openModal,
                 closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
                 {/* <button onClick={this.tester}>tester</button> */}
                 {this.props.children}
